8d8f263906e1244f398a32e9b2bd47b97cfabaec
Andressavcon <andressavconrado@gmail.com>
2026-03-31 21:37:36 -0300

    feat: setup cors, bean validation and fix jackson date serialization

--- src/main/java/com/andvc/eos/dto/EventResponseDTO.java
+++ src/main/java/com/andvc/eos/dto/EventResponseDTO.java
@@ -1,6 +1,7 @@
 package com.andvc.eos.dto;
 
 import com.andvc.eos.enums.EventStatus;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -8,6 +9,7 @@ import java.util.UUID;
 public record EventResponseDTO(
         UUID id,
         String name,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
         LocalDateTime date,
         EventStatus status
 ) {}
